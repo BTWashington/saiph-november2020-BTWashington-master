bb3271d12f6bbf7528f96c8c79870ea6be2bb645
Brandon Washington <BTWashington@student.fullsail.edu>
2020-11-17 19:38:35 -0500

    BWash Factory Method 3. Create Progress Bar

--- LevelState.cpp
+++ LevelState.cpp
@@ -72,7 +72,7 @@ namespace	// Global to this file only
 		ViewManager::GetInstance().AddObject(Temp, 0);
 		Temp->SetTexture("Resources/images/background.png");
 		Temp->SetDimensions(32.0f, 32);
-		Temp->SetColor(0.74f, 1.0f, 1, 1);
+		Temp->SetColor(0.74f, 1.0f, 1.0f, 1);
 		Temp->SetActive(true);
 
 		return Temp;
